2b9301a3b1c18120457fef529316f36391e0db45
danielbyun <dbyun@doinglab>
2020-07-19 22:23:19 +0900

    First unit test inside giftgiver

---
+++ giftgiver/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import { Grid, Typography } from "@material-ui/core";
+
+const App = () => {
+  return (
+    <Fragment>
+      <Grid container>
+        <Typography>Test</Typography>
+      </Grid>
+    </Fragment>
+  );
+};
+
+export default App;
